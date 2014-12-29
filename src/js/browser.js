730991fbbf0d71662e78765ac5b977c92c3bff35
Matt Cook <matt@zebradog.com>
2014-12-29 16:44:34 -0600

    on page load failure try to reload page once per second. fixes #13.

--- src/js/browser.js
+++ src/js/browser.js
@@ -1,9 +1,18 @@
 $(function(){
 
   var display;
-  
+
   chrome.storage.local.get('url',function(x){
-     $('#browser').attr('src',x["url"]).get(0).reload();
+     var restarting = false;
+     $('#browser').on('loadabort',function(e){
+       if(e.originalEvent.isTopLevel && !restarting){
+         restarting = true;
+         setTimeout(function(){
+           restarting = false;
+           $('#browser').get(0).reload();
+         },1000);
+       }
+     }).attr('src',x["url"]).get(0).reload();
   });
 
 });

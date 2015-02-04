4ec4356c165545ed5261befaabf4f5847c3d2f98
Matt Cook <matt@zebradog.com>
2015-02-04 16:21:00 -0600

    additional work on reset options UI

--- src/js/setup.js
+++ src/js/setup.js
@@ -1,6 +1,26 @@
 $(function(){
 
-  $("#host").on('change',hostChanged);
+  $("#host").on('change',function(){
+    if($("#host").val()){
+      $('.admin').hide().removeClass('disabled').slideDown();
+    }else{
+      $('.admin').slideUp();
+    }
+  });
+  $("#reset").on('change',function(){
+    if($("#reset").is(':checked')){
+      $('.reset').hide().removeClass('disabled').slideDown();
+    }else{
+      $('.reset').slideUp();
+    }
+  });
+  $("#restart").on('change',function(){
+    if($("#restart").is(':checked')){
+      $('.restart').hide().removeClass('disabled').slideDown();
+    }else{
+      $('.restart').slideUp();
+    }
+  });
 
   chrome.system.network.getNetworkInterfaces(function(interfaces) {
     for(var i in interfaces) {
@@ -15,24 +35,29 @@ $(function(){
 
   $('#url').focus();
 
-  function hostChanged(){
-    if($("#host").val()){
-      $('.admin').removeClass('disabled');
-    }else{
-      $('.admin').addClass('disabled');
-    }
-  }
+
 
   $('#save').click(function(e){
     e.preventDefault();
     var error = [];
     var url = $('#url').val();
     var host = $('#host').val();
+    var reset = $("#reset").is(':checked');
+    var restart = $("#restart").is(':checked');
     var port = parseInt($('#port').val());
+    var reset = $("#reset").is(':checked');
     port = port < 0 ? 0 : port;
     var username = $("#username").val();
     var password = $("#password").val();
     var passwordConfirm = $("#confirm_password").val();
+    if(reset){
+      var reset = parseFloat($('#resetinterval').val());
+      if(!reset) reset = 0;
+      if(reset <= 0 ){
+        reset = false;
+        error.push("Please specify reset interval");
+      }
+    }
     if(url && (url.indexOf("http://") >= 0 || url.indexOf("https://") >= 0 )){
       //url is valid
     }else{
@@ -60,6 +85,13 @@ $(function(){
         chrome.storage.local.set({'port':port});
         chrome.storage.local.set({'username':username});
         chrome.storage.local.set({'password':password});
+      }
+      if(reset){
+        chrome.storage.local.set({'reset':reset});
+      }
+      if(restart){
+        restart = parseInt($('#hour').val())+parseInt($('#houroffset').val());
+        chrome.storage.local.set({'restart':restart});
       }
       chrome.storage.local.set({'url':url});
       chrome.runtime.reload();

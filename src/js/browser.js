54a0cb7202d4a2467d8a753cbeb84b50f0d32d33
Matt Cook <matt@zebradog.com>
2015-05-18 12:49:44 -0500

    auto-reload on content crash or timeout

--- src/js/browser.js
+++ src/js/browser.js
@@ -1,18 +1,27 @@
 $(function(){
 
+  var RESTART_DELAY = 1000;
   var display;
 
   chrome.storage.local.get('url',function(data){
      var restarting = false;
-     $('#browser').on('loadabort',function(e){
-       if(e.originalEvent.isTopLevel && !restarting){
+
+     $('#browser').on('exit',function(e){ restart(); });
+     $('#browser').on('unresponsive',function(e){ if(e.originalEvent.isTopLevel) restart(); });
+     $('#browser')
+      .on('loadabort',function(e){ if(e.originalEvent.isTopLevel) restart(); })
+      .attr('src',data["url"]).get(0).reload();
+
+     function restart(){
+       if(!restarting){ 
          restarting = true;
          setTimeout(function(){
            restarting = false;
-           $('#browser').get(0).reload();
-         },1000);
-       }
-     }).attr('src',data["url"]).get(0).reload();
+           $('#browser').attr('src',data["url"]).get(0).reload();
+         },RESTART_DELAY);
+      }
+     }
+
   });
 
   chrome.runtime.onMessage.addListener(function(data){

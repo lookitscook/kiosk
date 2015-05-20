16555f655e77f9cbd2c93622666620e1d35c572b
Matt Cook <matt@zebradog.com>
2015-05-20 15:17:42 -0500

    improved handling of webview element

--- src/js/browser.js
+++ src/js/browser.js
@@ -1,23 +1,38 @@
 $(function(){
 
   var RESTART_DELAY = 1000;
-  var display;
 
   chrome.storage.local.get('url',function(data){
      var restarting = false;
 
-     $('#browser').on('exit',function(e){ restart(); });
-     $('#browser').on('unresponsive',function(e){ if(e.originalEvent.isTopLevel) restart(); });
-     $('#browser')
-      .on('loadabort',function(e){ if(e.originalEvent.isTopLevel) restart(); })
-      .attr('src',data["url"]).get(0).reload();
+     loadContent(data["url"]);
 
-     function restart(){
-       if(!restarting){ 
+     function loadContent(url){
+       $('<webview id="browser"/>')
+        .css({
+          width:'100%',
+          height:'100%',
+          position:'absolute',
+          top:0,
+          left:0,
+          right:0,
+          bottom:0
+        })
+        .attr('partition','persistant:kiosk')
+        .on('exit',onEnded)
+        .on('unresponsive',onEnded)
+        .on('loadabort',onEnded)
+        .attr('src',url)
+        .prependTo('body');
+     }
+
+     function onEnded(event){
+       if(!restarting){
          restarting = true;
+         $("#browser").remove();
          setTimeout(function(){
+           loadContent(data["url"]);
            restarting = false;
-           $('#browser').attr('src',data["url"]).get(0).reload();
          },RESTART_DELAY);
       }
      }

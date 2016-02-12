3a3a2193c2fac043ac10bf4a611663470ad14e20
seantrace <sean.trace@UKCAMD4686.aveva.com>
2016-02-12 11:08:50 +0000

    Add multi-display support

--- src/js/setup.js
+++ src/js/setup.js
@@ -1,9 +1,49 @@
 var DEFAULT_SCHEDULE_POLL_INTERVAL = 15; //minutes
 
+	function renderDisplayTemplate(display, id, isPrimary){
+		var scale = 0.09,
+		offsetX = 130,
+		offsetY = 0;
+		var info = currentDisplay.bounds;
+		
+		var $template = $('<div class="display-entry" style="padding-left:10px" title="Please select display to use."><div class="display-meta"></div></div>');
+			$template.css({
+				top: info.top*scale + offsetY,
+				left: info.left*scale + offsetX,
+				width: info.width*scale,
+				height: info.height*scale
+			}).data($.extend({id:id},display));
+
+			$template.find('.display-meta').text(info.width + 'x' + info.height);
+
+		if(isPrimary){
+			$template.addClass('selected');
+		}
+
+		return $template;
+	}
+
+	function displaySetup() {
+	 	chrome.system.display.getInfo(function(displayInfo) {
+			var $el;
+			$el = $('#display-setting-layer');
+			$el.empty();
+			for(var i=0; i<displayInfo.length; i++) {
+				currentDisplay = displayInfo[i];
+				template = renderDisplayTemplate(currentDisplay, i, currentDisplay.isPrimary);
+				$el.append(template);
+			}
+		});
+	}
+	
+$(function() {
+ displaySetup();
+ chrome.system.display.onDisplayChanged.addListener(displaySetup);
+});
+
 $(function(){
   chrome.storage.local.get(null,function(data){
     chrome.system.network.getNetworkInterfaces(function(interfaces) {
-
 
   for(var i in interfaces) {
     var interface = interfaces[i];
@@ -138,7 +178,7 @@ $(function(){
     var remoteschedule = $("#remote-schedule").is(':checked');
     var remotescheduleurl = $("#remote-schedule-url").val();
     var schedulepollinterval = parseFloat($('#schedule-poll-interval').val()) ? parseFloat($('#schedule-poll-interval').val()) : DEFAULT_SCHEDULE_POLL_INTERVAL;
-
+	
     if(reset){
       var reset = parseFloat($('#resetinterval').val());
       if(!reset) reset = 0;
@@ -227,17 +267,31 @@ $(function(){
       else chrome.storage.local.remove('disabletouchhighlight');
       if(disableselection) chrome.storage.local.set({'disableselection':disableselection});
       else chrome.storage.local.remove('disableselection');
+	  display = $('.display-entry.selected').data();
+	  chrome.storage.local.set({'display':display});
       chrome.storage.local.set({'url':url});
       chrome.runtime.reload();
     }
   });
-
+				  
   $('#demo').click(function(e){
     e.preventDefault();
     chrome.runtime.sendMessage('demo');
   });
 
-
-    });
+  $('.display-entry').click(function(e){
+    $target = $(e.target); 
+    $target.parent().find('.display-entry').removeClass('selected');	
+    $target.addClass('selected');
   });
+  
+  
+    });
+  });			  
+
+	   chrome.system.display.getInfo(function(displayInfo) {
+	var w = 0;var h = 0;
+  });
+  
 });
+

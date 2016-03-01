a63fda60e0f8332eb0a25ff873f6e32104a83268
Jacob Rosenberg <jake@lendup.com>
2016-02-29 16:53:44 -0800

    Create background.js

---
+++ src/js/background.js
@@ -0,0 +1,17 @@
+chrome.webRequest.onHeadersReceived.addListener(
+    function(info) {
+        var headers = info.responseHeaders;
+        for (var i=headers.length-1; i>=0; --i) {
+            var header = headers[i].name.toLowerCase();
+            if (header == 'x-frame-options' || header == 'frame-options') {
+                headers.splice(i, 1); // Remove header
+            }
+        }
+        return {responseHeaders: headers};
+    },
+    {
+        urls: [ '*://*/*' ], // Pattern to match all http(s) pages
+        types: [ 'sub_frame' ]
+    },
+    ['blocking', 'responseHeaders']
+);

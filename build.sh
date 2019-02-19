51a37e229d6c642b2d02f9f8d8d8e343ea8f0aff
Matt Cook <matt@cook.company>
2019-02-18 18:17:15 -0600

    mac dmg build

--- build.sh
+++ build.sh
@@ -1,47 +1,84 @@
 #!/usr/bin/env bash
 
+NAME=$(node -pe 'JSON.parse(process.argv[1]).name' "$(cat manifest.json)");
+VERSION_NAME=$(node -pe 'JSON.parse(process.argv[1]).version_name' "$(cat manifest.json)");
+
+# since we don't currently do this automatically anywhere else
 npm run beautify
 
-rm -rf dist/build;
-mkdir dist/build;
-mkdir dist/build/js;
-mkdir dist/build/css;
-mkdir dist/build/windows;
-mkdir dist/build/js/lib;
+# remove old build
+rm -rf dist;
+mkdir dist;
+mkdir dist/unpackaged;
+mkdir dist/unpackaged/js;
+mkdir dist/unpackaged/css;
+mkdir dist/unpackaged/windows;
+mkdir dist/unpackaged/js/lib;
 
 #build css
-cp -R lib/material-icons dist/build/css/material-icons
-cp node_modules/materialize-css/dist/css/materialize.min.css dist/build/css/materialize.min.css
-node-sass src/css/browser.scss --output-style compressed -o dist/build/css/
-node-sass src/css/shared.scss --output-style compressed -o dist/build/css/
-cp src/css/style.css dist/build/css/style.css
-cp src/css/ghpages-materialize.css dist/build/css/ghpages-materialize.css
+cp -R lib/material-icons dist/unpackaged/css/material-icons
+cp node_modules/materialize-css/dist/css/materialize.min.css dist/unpackaged/css/materialize.min.css
+node-sass src/css/browser.scss --output-style compressed -o dist/unpackaged/css/
+node-sass src/css/shared.scss --output-style compressed -o dist/unpackaged/css/
+cp src/css/style.css dist/unpackaged/css/style.css
+cp src/css/ghpages-materialize.css dist/unpackaged/css/ghpages-materialize.css
 
 #build html
-htmlmin src/windows/browser.html > dist/build/windows/browser.html
-htmlmin src/windows/setup.html > dist/build/windows/setup.html
+htmlmin src/windows/browser.html > dist/unpackaged/windows/browser.html
+htmlmin src/windows/setup.html > dist/unpackaged/windows/setup.html
 
 #build js
-cp node_modules/async/dist/async.min.js dist/build/js/async.min.js;
-cp node_modules/jquery/dist/jquery.min.js dist/build/js/jquery.min.js;
-cp node_modules/moment/min/moment.min.js dist/build/js/moment.min.js;
-cp lib/materialize-custom.min.js dist/build/js/materialize.min.js;
-cp lib/lodash.min.js dist/build/js/lodash.min.js;
-cp lib/wsc-chrome/wsc-chrome.js dist/build/js/lib/wsc-chrome.js;
-uglifyjs src/js/browser.js > dist/build/js/browser.min.js;
-uglifyjs src/js/main.js > dist/build/js/main.min.js;
-uglifyjs src/js/setup.js > dist/build/js/setup.min.js;
+cp node_modules/async/dist/async.min.js dist/unpackaged/js/async.min.js;
+cp node_modules/jquery/dist/jquery.min.js dist/unpackaged/js/jquery.min.js;
+cp node_modules/moment/min/moment.min.js dist/unpackaged/js/moment.min.js;
+cp lib/materialize-custom.min.js dist/unpackaged/js/materialize.min.js;
+cp lib/lodash.min.js dist/unpackaged/js/lodash.min.js;
+cp lib/wsc-chrome/wsc-chrome.js dist/unpackaged/js/lib/wsc-chrome.js;
+uglifyjs src/js/browser.js > dist/unpackaged/js/browser.min.js;
+uglifyjs src/js/main.js > dist/unpackaged/js/main.min.js;
+uglifyjs src/js/setup.js > dist/unpackaged/js/setup.min.js;
 
 #build assets
-cp -R src/img dist/build/img;
+cp -R src/img dist/unpackaged/img;
 
 #build meta
-cp manifest.json dist/build/manifest.json;
-cp schema.json dist/build/schema.json;
+cp manifest.json dist/unpackaged/manifest.json;
+cp schema.json dist/unpackaged/schema.json;
 
 # package it
-cd dist/build;
-zip -r "../$(node -pe 'JSON.parse(process.argv[1]).version_name' "$(cat manifest.json)").zip" .;zip -r "../$(node -pe 'JSON.parse(process.argv[1]).version_name' "$(cat manifest.json)").zip" .;
+cd dist;
+zip -r $VERSION_NAME-chrome-app.zip unpackaged;
 
 # build desktop versions
-build --tasks win-x86,win-x64,linux-x86,linux-x64,mac-x64 --mirror https://dl.nwjs.io/ --chrome-app .
+build --tasks win-x86,win-x64,linux-x86,linux-x64,mac-x64 --mirror https://dl.nwjs.io/ --chrome-app unpackaged;
+
+
+# create osx dmg
+cd mac-x64;
+echo "{
+  \"title\": \"$NAME\",
+  \"icon\": \"../unpackaged/img/icon.icns\",
+  \"contents\": [
+    { \"x\": 448, \"y\": 344, \"type\": \"link\", \"path\": \"/Applications\" },
+    { \"x\": 192, \"y\": 344, \"type\": \"file\", \"path\": \"$NAME.app\" }
+  ],
+  \"code-sign\": {
+      \"signing-identity\": \"3rd Party Mac Developer Application: Matthew Cook (5BNMYJ6L8S)\"
+  }
+}" > appdmg.json;
+node ../../node_modules/appdmg/bin/appdmg.js appdmg.json ../$VERSION_NAME-mac-x64.dmg;
+rm appdmg.json;
+cd ..;
+rm -rf mac-x64;
+
+# clean up
+rm -rf win-x64
+rm -rf win-x86
+mv win-x64-Setup.exe $VERSION_NAME-win-x64-setup.exe
+mv win-x86-Setup.exe $VERSION_NAME-win-x86-setup.exe
+zip -r $VERSION_NAME-linux-x86.zip linux-x86;
+rm -rf linux-x86;
+zip -r $VERSION_NAME-linux-x64.zip linux-x64;
+rm -rf linux-x64;
+
+
